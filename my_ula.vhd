4ec8c78e080b8e25dbaafb6cd8480338882e29e4
decoejz <decoejz@gmail.com>
2019-09-25 17:25:44 -0300

    add ula

--- my_ula.vhd
+++ my_ula.vhd
@@ -19,12 +19,18 @@ entity my_ula is
 	
 	port
 	(
+		-- Entradas
 		A : in std_logic_vector (DATA_WIDTH-1 downto 0);
 		B : in std_logic_vector (DATA_WIDTH-1 downto 0);
+		
+		-- Diz qual funcao sera executada.
 		func : in std_logic_vector (1 downto 0);
 		
+		-- Saida quando tem soma ou subtracao
 		Y : out std_logic_vector (DATA_WIDTH-1 downto 0);
-		Z : out std_logic
+		
+		-- Flag que indica se um numero e maior que outro
+		bigger_than : out std_logic
 	);
 end my_ula;
 
@@ -37,18 +43,31 @@ architecture ulaArch of my_ula is
 	
 begin
 
+	-- Funcoes da ULA
 	process(A,B,func)
 	begin
-		-- Add if "add_sub" is 1, else subtract
+		-- Soma A + B e o resultado é armazenado em Y.
 		if (func = "00") then
+			-- Transforma os std_logic_vector em inteiros positivos e soma.
+			-- Transforma de volta a resposta para std_logic_vector
 			Y <= std_logic_vector(unsigned(A) + unsigned(B));
 		
-		--elsif (func = "0001") then
-		--	if (A > B) then
-		--		Z <= '1';
-		--	else 
-		--		Z <= '0';
-		--	end if;
+		-- Compara se A > B
+		-- Retorna uma flag:
+		-- 1 caso seja maior,
+		-- 0 caso seja menor.
+		elsif (func = "01") then
+			if (A > B) then
+				bigger_than <= '1';
+			else 
+				bigger_than <= '0';
+			end if;
+			
+		-- Subtrai A - B e o resultado é armazenado em Y.
+		elsif (func = "10") then
+			-- Transforma os std_logic_vector em inteiros positivos e subtrai.
+			-- Transforma de volta a resposta para std_logic_vector
+			Y <= std_logic_vector(unsigned(A) - unsigned(b));
 		
 		end if;
 	end process;
